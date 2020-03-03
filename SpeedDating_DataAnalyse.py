d4eef2003f819e1335cc03f5722e3325270a201d
Valdemartt <36989028+Valdemartt@users.noreply.github.com>
2020-03-03 15:22:25 +0100

    Fixed load data function

--- SpeedDating_DataAnalyse.py
+++ SpeedDating_DataAnalyse.py
@@ -4,5 +4,9 @@ Created on Tue Mar  3 14:31:27 2020
 
 @author: valde
 """
-import load_data
-data = load_data()
+import os
+import sys
+sys.path.insert(0, os.getcwd())
+print(sys.path)
+from lib.DataLoad import load_data
+data = load_data("")
--- lib/DataLoad.py
+++ lib/DataLoad.py
@@ -4,10 +4,10 @@ Created on Tue Mar  3 14:19:31 2020
 
 @author: valde
 """
-def load_data(self, path):
+def load_data(path):
     import os 
     import pandas as pd
     csv_path = os.path.join(path, "Speed_Dating_Data.csv")
-    return pd.read_csv(csv_path)
+    return pd.read_csv(csv_path, encoding = "ISO-8859-1")
 
 
