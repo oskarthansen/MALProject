20976de0f223ef2803ae1627b46928b3eb147347
Valdemartt <36989028+Valdemartt@users.noreply.github.com>
2020-03-10 14:21:53 +0100

    Removed some columns in data

--- SpeedDating_DataAnalyse.py
+++ SpeedDating_DataAnalyse.py
@@ -10,5 +10,15 @@ sys.path.insert(0, os.getcwd())
 print(sys.path)
 from DataLoad import load_data
 data = load_data("Speed_Dating_data.csv")
+data.drop(['iid', 'id', 'idg', 'partner', 'pid', 'position', 'positin1', 'field_cd', 'undergra', 'tuition', 'from', 'zipcode', 'income', 'sports', 'tvsports', 'exercise', 'dining', 'museums', 'art', 'hiking', 'gaming', 'clubbing','reading', 'tv', 'theater', 'movies','concerts', 'music', 'shopping', 'yoga'], axis=1)
+
+
+corr = data.corr()
+from sklearn.metrics import confusion_matrix
+#conf_matrix = 
+match_corr = corr["match"].sort_values(ascending=False)
+
+
+
 
 
